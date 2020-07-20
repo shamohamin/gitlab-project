1cfa5891429c7d07ae37511493ba790a94d56e3e
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-07-20 22:53:10 +0430

    changed Connector.tsx

--- src/Connector.tsx
+++ src/Connector.tsx
@@ -1,44 +1,34 @@
 import React from "react";
-import {
-  HashRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-  RouteComponentProps,
-} from "react-router-dom";
-import LoginWrapper from "./componnets/LoginWrapper";
+// import {
+//   HashRouter as Router,
+//   Route,
+//   Switch,
+//   Redirect,
+//   RouteComponentProps,
+// } from "react-router-dom";
+// import LoginWrapper from "./componnets/LoginWrapper";
+import { Routes } from "./routes";
 
-type RouteSection = {
-  section: string;
-};
+// type RouteSection = {
+//   section: string;
+// };
 
 class Connector extends React.Component<{}, {}> {
-  private chooseComponent(routeProps: RouteComponentProps<RouteSection>) {
-    if (routeProps.match.params.section) {
-      switch (routeProps.match.params.section) {
-        case "login":
-          return <LoginWrapper />;
-        default:
-          return <Redirect to="/login" />;
-      }
-    } else {
-      return <Redirect to="/login" />;
-    }
-  }
+  // private chooseComponent(routeProps: RouteComponentProps<RouteSection>) {
+  //   if (routeProps.match.params.section) {
+  //     switch (routeProps.match.params.section) {
+  //       case "login":
+  //         return <LoginWrapper />;
+  //       default:
+  //         return <Redirect to="/login" />;
+  //     }
+  //   } else {
+  //     return <Redirect to="/login" />;
+  //   }
+  // }
 
   render() {
-    return (
-      <Router hashType="slash">
-        <Switch>
-          {}
-          <Route
-            path="/:section?"
-            render={(routeProps) => this.chooseComponent(routeProps)}
-          />
-          <Redirect to="/login" />
-        </Switch>
-      </Router>
-    );
+    return <Routes />;
   }
 }
 
