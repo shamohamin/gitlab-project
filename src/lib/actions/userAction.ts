8e431f670a6a38f00c89f8800daffe5c87c9fa6d
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-08-24 19:12:24 +0430

    added login singup actions

--- src/lib/actions/userAction.ts
+++ src/lib/actions/userAction.ts
@@ -10,10 +10,17 @@ import {
   AUTHENTICATION_SUCCEED,
   AuthenticationFaildAction,
   AUTHENTICATION_FAILD,
+  SIGN_UP,
+  SignUp,
 } from "./ActionTypes";
 import { Dispatch } from "redux";
 import { Users } from "../typesAndInterfaces/Users";
 import { RequestHandler } from "../RESTDATA/RequestHandlers";
+import { URLS } from "../RESTDATA/URLS";
+
+export const signUp: () => SignUp = () => ({
+  type: SIGN_UP,
+});
 
 export const logout: () => LogoutAction = () => ({
   type: LOGOUT,
@@ -39,16 +46,49 @@ export const userFetch = (user: Users): FetchUserAction => ({
   payload: user,
 });
 
-export const fetchUser = (psotdata: { [key: string]: string }, url: string) => {
+// sign up
+export const singUser = (postData: { [key: string]: any }, url: string) => {
   return async (dispatch: Dispatch<AppActions>) => {
     try {
-      const data = await new RequestHandler(url).postRequest({}, psotdata);
-      const user = data?.data as Users;
-      dispatch(login());
-      dispatch(userFetch(user));
-      dispatch(authenticateSuccess());
+      await new RequestHandler(url).postRequest({}, postData);
+      dispatch(signUp());
     } catch (ex) {
       dispatch(authenticateFailed(ex.toString()));
+      throw new Error("faild to sing try again.");
+    }
+  };
+};
+
+// sing in
+export const signIn = (postData: { [key: string]: any }, url: string) => {
+  return async (dispath: Dispatch<AppActions>) => {
+    try {
+      const token = await new RequestHandler(url).postRequest({}, postData);
+      dispath(login());
+      await retriveUser(token.data, URLS.RETRIVEUSER)(dispath);
+      dispath(authenticateSuccess());
+    } catch (ex) {
+      dispath(authenticateFailed(ex.toString()));
+      throw new Error("faild to sign in try again");
+    }
+  };
+};
+
+// retrive user
+export const retriveUser = (token: string, url: string) => {
+  return async (dispatch: Dispatch<AppActions>) => {
+    try {
+      const user = await new RequestHandler(url).postRequest(
+        {},
+        {},
+        {
+          Authorization: `Bearer ${token}`,
+        }
+      );
+      console.log(user);
+      dispatch(userFetch({ ...user.data, token: token } as Users));
+    } catch (ex) {
+      throw new Error(ex);
     }
   };
 };
