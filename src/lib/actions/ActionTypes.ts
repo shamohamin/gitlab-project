bbaa71178dc1604a30d80c953301ac8185fd2b46
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-08-24 19:08:56 +0430

    added Course actions and interfaces

--- src/lib/actions/ActionTypes.ts
+++ src/lib/actions/ActionTypes.ts
@@ -1,10 +1,16 @@
 import { Users } from "../typesAndInterfaces/Users";
+import { ICourse } from "../typesAndInterfaces/Course";
 
 export const LOGIN = "LOGIN";
 export const AUTHENTICATION_SUCCEED = "AUTHENTICATION_SUCCEED";
 export const AUTHENTICATION_FAILD = "AUTHENTICATION_FAILD";
 export const LOGOUT = "LOGOUT";
 export const FETCH_USER = "FETCH_USER";
+export const SIGN_UP = "SIGN_UP";
+
+export interface SignUp {
+  type: typeof SIGN_UP;
+}
 
 export interface LogoutAction {
   type: typeof LOGOUT;
@@ -33,6 +39,23 @@ export type UserActionTypes =
   | AuthenticationSucceedAction
   | FetchUserAction
   | LogoutAction
-  | AuthenticationFaildAction;
+  | AuthenticationFaildAction
+  | SignUp;
 
-export type AppActions = UserActionTypes;
+export const FETCH_COURSE_DATA = "FETCH_COURSE_DATA";
+export const EDIT_COURSE_DATA = "EDIT_COURSE_DATA";
+export const CREATE_COURSE_DATA = "CREATE_COURSE_DATA";
+
+export interface FetchCourseData {
+  type: typeof FETCH_COURSE_DATA;
+  payload: ICourse;
+}
+
+export interface EditCourseData {
+  type: typeof EDIT_COURSE_DATA;
+  payload: ICourse;
+}
+
+export type CourseActionTypes = EditCourseData | FetchCourseData;
+
+export type AppActions = UserActionTypes | CourseActionTypes;
---
+++ src/lib/typesAndInterfaces/Course.ts
@@ -0,0 +1,4 @@
+export interface ICourse {
+  data: string;
+  mode: "create" | "edit" | "read" | undefined;
+}
