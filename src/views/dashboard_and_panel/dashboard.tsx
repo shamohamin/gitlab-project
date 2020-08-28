de8f658ea3c3dcbb0e0e7cebe6a0a47ea02a6a42
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-08-29 02:46:45 +0430

    added Dashboard component Forms

---
+++ src/views/dashboard_and_panel/dashboard.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+// styles
+import "../../style/dashbaord.css";
+// interfaces
+import { interfaces } from "../interfaces";
+
+const SpanStyle: React.CSSProperties = {
+  paddingLeft: "10px",
+  paddingTop: "4px",
+  fontWeight: "bold",
+};
+
+export const Dashbaord: React.FC = () => {
+  const deafultState: interfaces.CourseType[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    deafultState.push({
+      name: "Advance programing",
+      instructor: "Dr. Esnashery",
+      startDate: "2020/09/03",
+      numberOfHomeWorks: Math.floor(Math.random() * 4) + 1,
+    });
+  }
+
+  const [courses] = React.useState<interfaces.CourseType[]>(deafultState);
+  return (
+    <>
+      <div className="dashbaord">
+        <div className="title">
+          <h2>Your Courses</h2>
+        </div>
+        <div className="content">
+          {courses.map((course: interfaces.CourseType, i: number) => (
+            <div key={i} className="item">
+              <div className="child">
+                <img src={require("../../images/course.jpg")} alt="pen" />
+                <h2>{course.name}</h2>
+                <div>
+                  <div>instructor: </div>
+                  <div style={SpanStyle}>{course.instructor}</div>
+                </div>
+                <div>
+                  <div>Start Date: </div>
+                  <div style={SpanStyle}>{course.startDate}</div>
+                </div>
+                <div>
+                  <div>Number Of Homeworks: </div>
+                  <div style={SpanStyle}>{course.numberOfHomeWorks}</div>
+                </div>
+              </div>
+              {Array.from({ length: course.numberOfHomeWorks! }).map(
+                (_, index: number) => (
+                  <div
+                    key={index}
+                    style={{
+                      zIndex: -(index + 1),
+                      top: (index + 1) * 4 + "px",
+                      left: (index + 1) * 4 + "px",
+                    }}
+                    className="child"
+                  ></div>
+                )
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
